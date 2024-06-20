89e229ebf65843c59844817ef070fcb0abafccd5
Hamza Kamelen <hamzakamelen12345@gmail.com>
2024-06-20 23:44:53 +0500

    Lab 7

---
+++ LAB - 7 (COAL).asm
@@ -0,0 +1,13 @@
+// 17 Statement Multiply two 8-bit numbers stored in memory locations 2200H and 2201H by repetitive addition  and store the result in memory locations 2300H and 2301H
+
+LDA 2200H
+MOV E,A
+MVI D, 00 : Get the first number in DE register pair
+LDA 2201H
+MOV C, A : Initialize counter
+LX I H, 0000 H : Result = 0
+BACK: DAD D : Result = result + first number
+DCR C : Decrement count
+JNZ BACK : If count 0 repeat
+SHLD 2300H : Store result
+HLT : Terminate program executio
